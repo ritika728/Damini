f3f399eb772ed79547486d76ee7f77151183ec3b
Ritika Malik <ritikamalik100102@gmail.com>
2022-11-12 20:42:13 +0530

    Updating MainActivity

--- app/src/main/java/com/example/damini/MainActivity.java
+++ app/src/main/java/com/example/damini/MainActivity.java
@@ -1,76 +1,171 @@
 package com.example.damini;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.damini.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
+    CardView siren, location, Settings, currentlocation, community, news, aboutUs, shareBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Intent backgroundService = new Intent( getApplicationContext(), ScreenOnOffBackgroundService.class );
+        this.startService( backgroundService );
+        Log.d( ScreenOnOffReceiver.SCREEN_TOGGLE_TAG, "Activity onCreate" );
+        int permissionCheck = ContextCompat.checkSelfPermission (MainActivity.this, Manifest.permission.SEND_SMS);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission (MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission (MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
 
-        setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.custom_dialog_mainactivity,null);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+            Button btn_okay = (Button)mView.findViewById(R.id.btn_okay);
+            TextView heading=mView.findViewById (R.id.heading);
+            heading.setText("LadyBuddy needs access to");
+            TextView sms=mView.findViewById (R.id.sms);
+            sms.setText("Sending SMS:-");
+            TextView textView=mView.findViewById (R.id.textFormodal);
+            textView.setText ("Emergency messaging needs SEND SMS permission");
+            TextView location=mView.findViewById (R.id.location);
+            location.setText("Location:-");
+            TextView locationText=mView.findViewById (R.id.textLocation);
+            locationText.setText("Messaging embedded with live location needs Location permission");
+            TextView call=mView.findViewById (R.id.call);
+            call.setText("Phone Call:-");
+            TextView callText=mView.findViewById (R.id.textCall);
+            callText.setText("Emergency Calling needs CALL PHONE permission");
+            TextView declaration=mView.findViewById (R.id.declaration);
+            declaration.setText("Declaration");
+            TextView declaratioText=mView.findViewById (R.id.textDeclaration);
+            declaratioText.setText("The app is solely developed by INDIAN Developers and all data related to this app is stored locally in your phone.");
+            CheckBox checkbox = (CheckBox)mView.findViewById(R.id.checkBox);
+            TextView checkBoxtext = (TextView)mView.findViewById(R.id.checkBoxText);
+            checkbox.setVisibility (View.VISIBLE);
+            checkBoxtext.setVisibility (View.VISIBLE);
+            checkbox.setEnabled (true);
+            checkBoxtext.setEnabled (true);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            checkbox.setText("");
+            checkBoxtext.setText(Html.fromHtml("I accept the " +
+                    "<a href='https://www.websitepolicies.com/policies/view/IaK4RjyB'>PRIVACY POLICY</a>"+" of the app"));
+            checkBoxtext.setClickable(true);
+            checkBoxtext.setMovementMethod(LinkMovementMethod.getInstance());
+            alert.setView(mView);
+            final AlertDialog alertDialog = alert.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            btn_okay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(checkbox.isChecked ()) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                        alertDialog.dismiss ();
+                        ActivityCompat.requestPermissions (MainActivity.this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CALL_PHONE}, 0);
+                    }else{
+                        Toast.makeText (MainActivity.this,"Please accept privacy policy",Toast.LENGTH_LONG).show ();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    }
+                }
+            });
+            alertDialog.show();
+
         }
 
-        return super.onOptionsItemSelected(item);
+        siren = findViewById( R.id.Siren );
+        location = findViewById( R.id.send_Location );
+        Settings = findViewById( R.id.Settings );
+        currentlocation = findViewById( R.id.Currentlocation );
+        news = findViewById( R.id.News );
+        aboutUs = findViewById( R.id.about_us );
+        // community=findViewById (R.id.community);
+        siren.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( getApplicationContext(), Flashing.class ) );
+            }
+        } );
+        location.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( getApplicationContext(), Instructions.class ) );
+            }
+        } );
+        Settings.setOnClickListener( v -> startActivity( new Intent( getApplicationContext(), SmsActivity.class ) ) );
+        currentlocation.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( getApplicationContext(), ChoosenActivity.class ) );
+            }
+        } );
+        news.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( getApplicationContext(), NewsActivity.class ) );
+            }
+        } );
+
+
+        aboutUs.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( getApplicationContext(), AboutUs.class ) );
+            }
+
+        } );
+
+        shareBtn = findViewById(R.id.ShareBtn);
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "LadyBuddy");
+                    String shareMessage= "Hey Ladies,I am gifting a token of safety to all the females in my society as \n\n*LadyBuddy* solves a very heart wrenching problem of our civilisation, *Women's Safety*. \n\nJust *download*,start using, and spread the app \n\nSo that any *female* related to you can feel safer and empowered in this world. \n\nDownload LadyBuddy at:-\n";
+
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID ;
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "Share"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
+            }
+        });
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
+
+
 }
